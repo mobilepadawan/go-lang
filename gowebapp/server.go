69477973349e33a508775ce7109090bc4528cd46
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-08-24 11:39:44 -0300

    adding static content

--- gowebapp/server.go
+++ gowebapp/server.go
@@ -1,9 +1,13 @@
 package main
 
+//importamos la librería NET/HTTP para crear nuestro servidor
 import (
 	"net/http"
-	)
+)
 
+//Creamos nuestra función Main (inicial) para levantar nuestro servidor web en Go
 func main() {
-	http.ListenAndServe( ":8080", nil )
+	fs := http.FileServer(http.Dir("./static/"))             //definimos el fichero fileServer y le indicamos que, el directorio de los archivos estáticos es la carpeta STATIC. Todo queda guardado en la variable fs, correspondiente a file system
+	http.Handle("/static", http.StripPrefix("/static/", fs)) //Luego, con handle le decimos al servidor web que, cuando lleguen peticiones de contenido estático, sirva las mismas a través del prefijo indicado stripPrefix()
+	http.ListenAndServe(":8080", nil)
 }
